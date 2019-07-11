b01c781f189f5c67739f9b3a23d0f2e0371b2983
rosalesnikho <rosales.nikho@gmail.com>
2019-07-11 17:56:27 -0500

    init

---
+++ src/main/java/pro/tixie/tixie/TixieApplication.java
@@ -0,0 +1,13 @@
+package pro.tixie.tixie;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TixieApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TixieApplication.class, args);
+    }
+
+}
---
+++ src/test/java/pro/tixie/tixie/TixieApplicationTests.java
@@ -0,0 +1,16 @@
+package pro.tixie.tixie;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class TixieApplicationTests {
+
+    @Test
+    public void contextLoads() {
+    }
+
+}
