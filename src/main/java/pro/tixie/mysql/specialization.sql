d8970d821228cf0e1b042e25cc754a677ead58f6
John De Leon <johndeleon@Johns-MacBook-Pro.local>
2019-07-13 17:56:38 -0500

    adding manager controllers

--- src/main/java/pro/tixie/mysql/specialization.sql
+++ src/main/java/pro/tixie/mysql/specialization.sql
@@ -3,4 +3,4 @@ insert into specialization (id, name) values (2, 'electrical');
 insert into specialization (id, name) values (3, 'plumbing');
 insert into specialization (id, name) values (4, 'hvac');
 insert into specialization (id, name) values (5, 'custodian');
-insert into specialization (id, name) values (6, 'default');
+insert into specialization (id, name) values (6, 'biohazard');
