6da0fdee9e972ce04ae2cf24d597604993644e2d
JohnDeLeon894 <johnmdeleon894@gmail.com>
2019-07-14 22:06:04 -0500

    added dash controller and view for stats based on ticket status

--- src/main/java/pro/tixie/controllers/user/managercontrollers/ManagerDashController.java
+++ src/main/java/pro/tixie/controllers/user/managercontrollers/ManagerDashController.java
@@ -7,6 +7,8 @@ import org.springframework.web.bind.annotation.PathVariable;
 import pro.tixie.model.*;
 import pro.tixie.repos.*;
 
+import java.util.List;
+
 @Controller
 public class ManagerDashController {
 
@@ -31,7 +33,33 @@ public class ManagerDashController {
 
     @GetMapping("dashboard/all")
     public String dashAll(Model model){
-        return "full dash view";
+//        all graphs will just be numerical stats for now
+//        one section for stats of all tickets
+//        one graph for tickets by status
+//        open = 1 in progress = 2 complete = 3
+        Status comp = statDao.findById(3);
+        Status inProg= statDao.findById(2);
+        Status open= statDao.findById(1);
+
+        List<Ticket> allTix = ticketDao.findAll();
+        List<Ticket> compTix = ticketDao.findAllByStatus(comp);
+        List<Ticket> inProgTix= ticketDao.findAllByStatus(inProg);
+        List<Ticket> openTix= ticketDao.findAllByStatus(open);
+
+//        String totalTix=
+
+        model.addAttribute("totalTix", allTix.size() );
+        model.addAttribute("complete", compTix.size());
+        model.addAttribute("completePerc",percentCalc(compTix.size(), allTix.size()));
+        model.addAttribute("inProg", inProgTix.size());
+        model.addAttribute("inProgPerc", percentCalc(inProgTix.size(), allTix.size()));
+        model.addAttribute("open", openTix.size());
+        model.addAttribute("openPerc", percentCalc(openTix.size(), allTix.size()));
+//        one graph for tickets by catagory
+//        one graph for tickets by time line.
+
+
+        return "user/manager/dashboard";
     }
 
     @GetMapping("dashboard/roles/{id}")
@@ -42,6 +70,12 @@ public class ManagerDashController {
     @GetMapping("dashboard/specialization/{id}")
     public String dashSpecialization(@PathVariable long id,Model model){
         return "Spec dash view";
+    }
+
+//    Helper methods
+
+    public double percentCalc(double tix, double totaltix) {
+        return tix * 100 / totaltix;
     }
 
 }
