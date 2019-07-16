4189be740a084d4815ac69c6cfc7652bfd435d89
JohnDeLeon894 <johnmdeleon894@gmail.com>
2019-07-16 14:04:40 -0500

    merged changes

--- src/main/java/pro/tixie/repos/UserRepository.java
+++ src/main/java/pro/tixie/repos/UserRepository.java
@@ -2,10 +2,18 @@ package pro.tixie.repos;
 
 import org.springframework.data.repository.CrudRepository;
 import org.springframework.stereotype.Repository;
+import pro.tixie.model.Role;
+import pro.tixie.model.Specialization;
 import pro.tixie.model.User;
+
+import java.util.List;
 
 @Repository
 public interface UserRepository extends CrudRepository<User, Long> {
     User findByUsername(String username);
+
+    public List<User> findAll();
+    public List<User> findAllByRole( Role role);
+    public List<User> findAllBySpecialization(Specialization spec);
 
 }
