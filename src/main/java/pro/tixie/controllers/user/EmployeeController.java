e5db95852c55af9aaa3aca9aa579b35d9b7875c8
rosalesnikho <rosales.nikho@gmail.com>
2019-07-16 12:23:33 -0500

    added view logic

--- src/main/java/pro/tixie/controllers/user/EmployeeController.java
+++ src/main/java/pro/tixie/controllers/user/EmployeeController.java
@@ -1,4 +1,5 @@
 package pro.tixie.controllers.user;
+import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -7,6 +8,8 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import pro.tixie.model.*;
 import pro.tixie.repos.*;
+
+import java.util.List;
 
 @Controller
 public class EmployeeController {
@@ -33,19 +36,22 @@ public class EmployeeController {
     @GetMapping("/employee/{id}")
     public String allTixByEmployee(@PathVariable long id, Model model) {
         User userTix = userDao.findOne(id);
+        Ticket tixNote = ticketDao.findOne(id);
+        List<Note> note = noteDao.findAllByTicket(tixNote);
         model.addAttribute("tix", ticketDao.findAllByAuthor(userTix));
         model.addAttribute("employee", userDao);
+        model.addAttribute("note", note);
         return "user/employee/index";
     }
 
     @RequestMapping("/employee/ticket/create")
-    public String showTicketCreate(Model model) {
+    public String showTicketCreate(Model model, Status status, Specialization spec, Priority prior, Note note) {
         model.addAttribute("tix", new Ticket());
-        model.addAttribute("status", new Status());
-        model.addAttribute("spec", new Specialization());
-        model.addAttribute("priority", new Priority());
-        model.addAttribute("notes", new Note());
-        return "user/employee/index";
+        model.addAttribute("status", status);
+        model.addAttribute("spec", spec);
+        model.addAttribute("priority", prior);
+        model.addAttribute("note", note);
+        return "user/employee/create_ticket";
     }
 
     @PostMapping("/employee/ticket/create/{id}")
