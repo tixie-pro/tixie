ad4c802c2db3da3c2cf0ede382c489b377298419
rosalesnikho <rosales.nikho@gmail.com>
2019-07-17 15:26:52 -0500

    added-login-nav-and-links

--- src/main/java/pro/tixie/repos/SecurityConfiguration.java
+++ src/main/java/pro/tixie/repos/SecurityConfiguration.java
@@ -40,7 +40,7 @@ public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
                 /* Login configuration */
                 .formLogin()
                 .loginPage("/login")
-                .defaultSuccessUrl("/employee") // user's home page, it can be any URL
+                .defaultSuccessUrl("/") // user's home page, it can be any URL
                 .permitAll() // Anyone can go to the login page
                 /* Logout configuration */
                 .and()
