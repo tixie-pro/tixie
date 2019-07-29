d9e8076001067a0e60c32ddf8875f1809366b7d3
JohnDeLeon894 <johnmdeleon894@gmail.com>
2019-07-28 23:28:31 -0500

    changed biohazard in controller

--- src/main/java/pro/tixie/controllers/user/managercontrollers/ManagerDashController.java
+++ src/main/java/pro/tixie/controllers/user/managercontrollers/ManagerDashController.java
@@ -72,7 +72,9 @@ public class ManagerDashController {
         List<Ticket> plumTix = ticketDao.findAllBySpecialization(plum);
         List<Ticket> hvacTix = ticketDao.findAllBySpecialization(hvac);
         List<Ticket> custTix = ticketDao.findAllBySpecialization(cust);
-        List<Ticket> biohTix = ticketDao.findAllByBiohazardEquals(1);
+        List<Ticket> biohTix = ticketDao.findAllBySpecialization(bioh);
+
+//        List<Ticket> biohTix = ticketDao.findAllByBiohazardEquals(1);
 
 
         model.addAttribute("techs", allTechs );
